761d7435e28faee57c614fb08c908c1d2d0da4b9
namtran0110 <namtran0110@gmail.com>
2016-10-17 10:06:34 +0700

    Main Page fix

--- silisili/settings.py
+++ silisili/settings.py
@@ -91,17 +91,18 @@ WSGI_APPLICATION = 'silisili.wsgi.application'
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        # 'ENGINE': 'django.db.backends.sqlite3',
-        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'postgres',
-        'USER': 'dk',
-        'PASSWORD': '1',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+            # 'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            # 'NAME': 'postgres',
+            # 'USER': 'dk',
+            # 'PASSWORD': '1',
+            # 'HOST': '127.0.0.1',
+            # 'PORT': '5432',
+        }
     }
-}
+
 
 # Password validation
 # https://docs.djangoproject.com/en/1.10/ref/settings/#auth-password-validators
